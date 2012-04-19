36f71fa113d8effcc4dcbbd230405032a6fb1502
jtimberman <joshua@opscode.com>
2012-04-19 10:46:13 -0600

    [COOK-1188] - use the gunicorn_install lwrp in the recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,13 +20,6 @@
 
 include_recipe 'python'
 
-unless node["gunicorn"]["virtualenv"].nil?
-  python_virtualenv node["gunicorn"]["virtualenv"] do
-    action :create
-  end
-end
-
-python_pip "gunicorn" do
-  virtualenv node["gunicorn"]["virtualenv"] unless node["gunicorn"]["virtualenv"].nil?
-  action :install
+gunicorn_install "gunicorn" do
+  virtualenv node['gunicorn']['virtualenv']
 end
