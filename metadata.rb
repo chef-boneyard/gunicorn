3fbc22f6428cadb2405f81e30be8dd0bcd94c06b
jtimberman <joshua@opscode.com>
2012-08-31 10:33:14 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures Gunicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 
 depends "python"
