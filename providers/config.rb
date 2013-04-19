201774d4041184497f47124a086cc41435a9f354
Julien Phalip <jphalip@gmail.com>
2013-04-19 12:05:16 -0700

    [COOK-3313] Added attributes for Gunicorn's log settings.

--- providers/config.rb
+++ providers/config.rb
@@ -19,23 +19,23 @@
 #
 
 action :create do
-  
+
   Chef::Log.info("Creating #{@new_resource} at #{@new_resource.path}")  unless exists?
-  
+
   template_variables = {}
-  %w{listen backlog preload_app worker_processes worker_class worker_timeout worker_keepalive worker_max_requests server_hooks pid}.each do |a|
+  %w{listen backlog preload_app worker_processes worker_class worker_timeout worker_keepalive worker_max_requests server_hooks pid accesslog access_log_format errorlog loglevel logger_class logconfig}.each do |a|
     template_variables[a.to_sym] = new_resource.send(a)
   end
-  
+
   Chef::Log.debug("Using variables #{template_variables} to configure #{@new_resource}")
-  
+
   config_dir = ::File.dirname(new_resource.path)
-  
+
   d = directory config_dir do
     recursive true
     action :create
   end
-  
+
   t = template new_resource.path do
     source new_resource.template
     cookbook new_resource.cookbook
@@ -44,11 +44,11 @@ action :create do
     group new_resource.group if new_resource.group
     variables template_variables
   end
-  
+
   new_resource.updated_by_last_action(d.updated_by_last_action? || t.updated_by_last_action?)
 end
 
-action :delete do 
+action :delete do
   if exists?
     if ::File.writable?(@new_resource.path)
       Chef::Log.info("Deleting #{@new_resource} at #{@new_resource.path}")
