e235499e4bbdbfec968aae570c3f88d4dfc14ab7
Christopher Webber <cwebber@getchef.com>
2014-05-28 05:14:06 -0700

    Version bump to 1.1.4

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures Gunicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.3"
+version          "1.1.4"
 
 depends "python"
