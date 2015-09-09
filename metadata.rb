3292c83389ca90eded15d7db9a3cdfc5c838a0e1
Tim Smith <tsmith84@gmail.com>
2015-09-08 20:55:21 -0700

    Opscode -> chef, add platforms, add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,16 @@
 name             "gunicorn"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license          'Apache 2.0'
 description      "Installs/Configures Gunicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.2.0"
 
 depends "python"
+
+%w(amazon centos debian fedora oracle redhat scientific ubuntu).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/gunicorn' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/gunicorn/issues' if respond_to?(:issues_url)
