81928ba408173f6ef81031dab9f90889ebd0252f
Tim Smith <tsmith@chef.io>
2016-09-16 10:54:05 -0700

    Improve specs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -4,7 +4,7 @@ describe 'gunicorn::default' do
   let(:gunicorn_virtualenv) { '/srv/gunicorn/test' }
 
   cached(:chef_run) do
-    ChefSpec::ServerRunner.new do |node|
+    ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04') do |node|
       node.normal['gunicorn']['virtualenv'] = gunicorn_virtualenv
     end.converge(described_recipe)
   end
